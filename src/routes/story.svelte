a2421e97d3e2cb0856c95d3e68ce2c105b7490bb
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-02-11 13:20:03 -0800

    Add files via upload

---
+++ src/routes/story.svelte
@@ -0,0 +1,48 @@
+<body>
+    
+    
+    <img src="img/purp.png" alt="">
+
+    <h1>Our Story</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, nostrum tempora? Similique natus optio necessitatibus quis adipisci rerum dicta quaerat.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam molestias sed unde adipisci itaque vero, eligendi non eaque, repellat magni impedit, quas doloremque illum voluptatibus optio ipsam quasi fugit rem!</p>
+
+    <img src="img/purp.png" alt="">
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et maxime ipsam qui amet dolore obcaecati corrupti! Ducimus, exercitationem!</p>
+
+    <img src="img/purp.png" alt="" style="margin-bottom: 120px;">
+
+    <h1>The Band</h1>
+
+    <a href="/dane">
+    <img src="img/purp.png" alt="">
+
+    <h1>Dane <br> </h1></a>
+    
+    <a href="/dane">
+    <img src="img/purp.png" alt="">
+
+    <h1>This <br> person  </h1></a>
+    
+    <a href="/dane">
+    <img src="img/purp.png" alt="">
+
+    <h1>That <br>  Person</h1></a>
+    
+    <a href="/dane" >
+    <img src="img/purp.png" alt="">
+
+    <h1 style="margin-bottom: 120px;">Other<br>  Person</h1></a>
+</body>
+
+
+<style lang="scss">
+    @import 'components';
+
+    img {
+        width: 100%;
+    }
+
+</style>
