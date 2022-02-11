fb0905d80168108394e5d767269d4d8054263588
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-02-11 13:41:16 -0800

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,9 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
-import sveltePreprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: sveltePreprocess(),
 	kit: {
-		adapter:adapter(),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
