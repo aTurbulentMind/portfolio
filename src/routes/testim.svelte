13f0bb58bd62c06cce3746928fc7429955a38e9d
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-02-16 14:20:38 -0800

    Add files via upload

--- src/routes/testim.svelte
+++ src/routes/testim.svelte
@@ -2,21 +2,21 @@
     <h1 class="altFont"  >Testimonials</h1>
 
     <div class="social">
-       <a href="https://debtnet5build.netlify.app/">  <img src="img/debtBanner.png/" alt=""></a> 
+       <a href="https://debtnet5build.netlify.app/">  <img src="img/debtBanner.png/" alt="screenshot of a website build for Debt Net"></a> 
     </div>
 
     <h2 class="altFont">Debt Net$</h2>
     <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus doloribus corporis quisquam perspiciatis? Expedita dolore unde ullam quisquam, repellat consectetur? Veniam in debitis hic quisquam!" &nbsp; &nbsp; &nbsp;  </p>
 
     <div class="social">
-       <a href="https://harboreddreambuild.netlify.app/">  <img src="img/harbBanner.png/" alt=""></a> 
+       <a href="https://harboreddreambuild.netlify.app/">  <img src="img/harbBanner.png/" alt="screenshot of a website build for Harbored Dreams"></a> 
     </div>
     
     <h2 class="altFont">Harbored Dreams</h2>
     <p style="margin-bottom: 30%;">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta alias quaerat doloremque! Qui debitis ad similique quibusdam recusandae consequuntur quaerat vitae illo!" &nbsp; &nbsp; &nbsp; </p>
 
     <div class="social">
-       <a href="https://oathbuild.netlify.app/">  <img src="img/oathBanner.png/" alt=""></a> 
+       <a href="https://oathbuild.netlify.app/">  <img src="img/oathBanner.png/" alt="screenshot of a website build for oath"></a> 
     </div>
     
     <h2 class="altFont">Orca Art Tattoo House</h2>
