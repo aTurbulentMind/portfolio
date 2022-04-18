e1a283381a97e139f731f8b3fbfbcfe48112e682
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-04-18 09:28:07 -0700

    Add files via upload

---
+++ src/routes/blog/whoru.svelte
@@ -0,0 +1,110 @@
+<body>
+    
+<h1>Who r U?</h1>
+
+        
+        <img src="/img/mixed2.png" alt="">
+
+        
+    <!--   <p>
+                Save your sight. You can listen instead.
+        </p>
+        <audio controls>
+        <source src="/sound/stillAliveMufuka.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+        </audio>
+--> 
+
+<p>
+   We are all strangers. Unknown and barely visible. Another randomly generated face. That is until you extend a simple word, "hello" or whatever the greeting in your native tongue maybe.  Then followed by your name and a smile. Blammo! Broke the frozen wall of "who are you" in a few seconds flat. If you don't like that, you may not want to be near me in the grocery line. I love to talk to strangers. Not sure why. Something about how they could have a sentence tucked away just for me. A phrase that will reshape some avenue of thought.
+</p>
+   <p>
+  So hello. I'm Joel. You may know that, or you may not. It is super likely nobody will take the moment to get this far. On the opposite. If you see these words. Thanks and stuff like that. I am going to talk about myself for a few moments. The first article I wrote in this collection felt like a "why" I was finally starting. I missed the who part. Do you know the who, what, where, why?
+</p>
+    <p><span> "I am not the best at following rules. I can only be so sorry about it."</span></p>
+  <p>
+  I am not the best at following rules. Yeah. I can only be so sorry about it. I want to do the right thing. That just means what I think is right and what the rules say are not quite simpatico at all moments in our timestream. I used to resent it, wondering why I could not just fall in line like all of the other lemmings. Why do so many others not seem effected by negative treatment from management? Why must I oppose any question? Maybe I was born like that? I have left and been fired from jobs over my lack of shock at finger-wagging overseers. 
+</p>
+  <p>
+  That probably came from my super awesome wonderfully fulfilled and complete childhood. I for sure grew up with both parents and good schooling. They had a lot of money and we went on vacations all the time. The house was big and comfortable while nestled in a nice quaint little cul-de-sac. We were nice to each other. Nobody every yelled because we loved each other so much. It was perfect. 
+</p>
+
+    <img class="port" src="/img/silenceIsGoldenexpo.png" alt="">
+
+  <p>
+  At least that’s the fantasy I lived in within my head. People around me yelled and fought and did drugs. They stayed awake for days at a time in whatever random house I found myself sleeping on the floor of. My vacation was a fancy shelled white wall of bricks, the most chic orange one piece and all the seclusion 23 solitary hours a day could provide. It was easier for my mother or foster parents to call the cops than it was to just care. That father, he was imaginary. I did see a picture one time when I was four though. 
+</p>
+  <p>
+ I sent my spit away to ancestry a few weeks ago. I might finally fix that blank image. That would be cool and confusing in the same breath. Almost 32 years and no contact with half of my creators. There are times in my life that I felt him not being there made me less than ok. I thought my problems came from the lack of anyone to fill that role. Yes an invisible man leaves an empty dictionary when seeking definition or answers. No I do not have to let it corrode my best traits. And neither do you. A lack of siblings, one or both parents does not make you less human. It in fact is what makes us. The struggles and problems we survived are used in how we overcome every day.
+</p>
+
+    <p><span>"Eight billion people on this planet. I don't communicate with people I don’t want to. Why would I?"</span></p>
+
+ <p>
+ The other maker is pretty much dead to me. You can only chose drugs over your child so many times before you die. She walks and breathes, but might as well not to me. Eight billion people on this planet. I don't communicate with people I don’t want to. Why would I? Because they are "family"? That is only a word when the weight of love and time do not exist to give it any meaning. She abandoned me at 16. I tried a few times after that to care. As much trouble I felt inside, our connection lost signal. It was like using those electric paddles doctors use that zap people back to life on a corpse you just dug out of a grave. As strange as the world is, one of my friends dad has been with her for some years. 
+</p>
+ <p>
+  That childhood really had an impact on my forever. There are trauma's that will last even through years of therapy. I wish I could have just deleted them before becoming a parent in 2010. I messed up the chance for my kid's to have some picture perfect nuclear family image that might hang on their wall when they are parents themselves. The wife at the time and I had grown so far apart. And I was an avid conspiracy theorist. And I lived at work. And she like to drink. And liked to hit me or throw shit at me. At some point, it is just best for the kids to not see that. If she wants to live that way I cant stop her. But that does not mean I have to live through it. I may love a bit of aggressive play when the time and place call for it. But that does not mean I have to let myself be attacked out of anger.
+</p>
+
+  <p>
+ The kids made it so difficult to leave. I had no parents growing up and I anguished over the thought of my own without us together. Like it would break them leaving nothing but prison. That is simply not how it works. Life is not that simple.  Just because we are separated does not mean I am going to ditto my dad and dip. I still get to be in their life and influence them in the ways that I can. We still get to be best friends. I had to repeat a mantra of "we have our whole life to hang out".
+</p>
+
+    <img class="port" src="/img/meanmeow.jpg" alt="">
+
+ <p>
+  I found someone who deals with me. SHe is great, but I wandered a bit almost as lost as I am right now. I had to learn how to be an adult and enjoy being by myself. I seemed to have leaned in a bit to hard on that self reliance to the point that nobody reaches out. I inadvertently became a ghost. And that should be ok. I have enough to learn around me to keep me distracted from how I feel. Today I wonder who I am. I  am not quite sure it matters or that I care. But I do know I have to do something. I have to keep moving and learning. I can not just quit. That would give me way to much extra time to hate myself. So I am at least gonna keep myself productive so that I don’t have time to try anything brash.
+</p>
+  <p>
+  I am so much more than a few words can describe. I skipped hobbies and interest and just gave a bit of me to start. Next week I will write about where I am and what I do. Until then take care, I love you.
+</p>
+
+<img class="port" src="/img/outdoor.jpg" alt="">
+
+</body>
+
+<style lang="scss"> 
+    
+    @import '../src/routes/_components.scss';
+
+    img{
+        max-width: 80vw;
+        margin: 0 15%;
+    }
+
+    .port {
+        max-width: 100vw;
+        margin: 0 40%;
+    }
+
+    audio{
+        margin: -9% 30% 0 30%;
+        box-shadow: 0 0 10px rgba(180, 10, 232, 0.5);
+        border-radius: 50%;
+    }
+
+    p{
+        margin: 9% 18%;
+
+        span{
+            font-weight: bold;
+            text-decoration: underline purple 2px;
+            text-transform: italic;
+        }
+    }
+
+      @media (max-width: 768px) {
+      p{
+          margin: 9% 7%;
+      }
+      img{
+        max-width: 80vw;
+        margin: 5%;
+        }
+      .port {
+        max-width: 80vw;
+        margin: 4% 10%;
+        }
+      }
+</style>
