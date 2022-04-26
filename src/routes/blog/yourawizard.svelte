f46aa3ca99fc62f63272f286206be1d36eae4974
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-04-26 10:58:28 -0700

    Add files via upload

---
+++ src/routes/blog/yourawizard.svelte
@@ -0,0 +1,108 @@
+<body>
+    
+<h1>Gotta Start somewhere</h1>
+
+        
+        <img class="port" src="/img/wizard.jpg" alt="encrouching siamese cat">
+
+        
+    <!--   <p>
+                Save your sight. You can listen instead.
+        </p>
+        <audio controls>
+        <source src="/sound/stillAliveMufuka.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+        </audio>
+-->
+
+<p><span> Visual content info: We went to live music this week and I made a cool digital doughnut.</span></p>
+
+<p>
+    Hi! Welcome back. Or hello for the first time ever. Whatever. I still love you for being a human.  Makes it difficult if you are an ass. But also, whatever, you deserve love. And that should include loving yourself. I have not always been the valedictorian in the school of internal adulation. So I have to keep myself busy to stray away from my own self depreciative reasoning. In some sacrificial unending quest I have found a few ways to escape from the problems around me. 
+</p>
+
+<p>
+  Isn't that what life is? Finding ways to run from this or that? No. the answer should be no to that question. It has to be more of a conversation with yourself. You have to really talk to you and find out if this outlet is healthy or at least non harmful? MY first lines I was hooked. To sit down, crack it open and instant rush. It demanded the highest price. And I gave it. I let go of my freedom. I let go of myself. I gave in. I really had to have a heart to heart with myself as I sat in the dark, tired eyes hidden behind the hoodie I put on yesterday. Is this what I want out of my life? 
+</p>
+
+<img src="/img/wizardtwo.jpg" alt="Mayday the best band">
+
+<p>
+  Yes. Yes I do. Writing code changed my life and continues to improve it. Or am I gaslighting the man in the mirror? It was really easy for me to beat myself down. And even after years of crushing my face into the wall learning that next thing, I still have to keep trying. It started at a shop I worked at and it spun into an almost addiction. I would rise so excited to at least have something to try. Something I could never become the master of. Then be into the hours of night glued to my task. Still a marathon from complete in my checklist. I needed something that cannot be completed. Because I easily get bored. I have an issue with breaking down how things work to understand them. Unfavorably often I am disinterested after my query. In code I only found more questions. Almost four years later and I am still so curious. Still finding things that make me say "oooh".
+</p>
+<p><span>"it does not even matter if I agree or not I just seem to have to argue. And I love it. I need people to have opposing ideas to me."</span></p>
+<p>
+  So what do I do? I create paths for people to reach new levels of conversion. I do so through the creation of websites. It is a way for me to give back to people and be self sufficient. Because I am just not good at being an employee. I ask too many questions and talk back almost every chance I get. Like, it does not even matter if I agree or not I just seem to have to argue. And I love it. I need people to have opposing ideas to me. I Have to have people willing to debate. Either strengthen my idea or convince me otherwise. Most of the time I would be arguing for the betterment of humans and what that would look like. Instead of talking back about how people deserve to be treated, I have to make a way to help the people I think are so taken advantage of. So I gave in and chose to try the freelance route. It has to be better than my experience with looking for work from a tech company. I feel sick to my stomach over that idea.
+</p>
+<img src="/img/wizardfour.jpg" alt="joey cool on stage being great">
+<p>
+ For most of the time I have felt like an imposter over what I can or cannot do. I have always had an issue with admitting my level of skill. It was listening to a Dr Drew podcast that I realized my worth. I know that sounds funny or ridiculous, but to hear a doctor talk about how he inside thinks that he will never be smart or good enough changed me. To hear someone with a doctorate degree second guess themselves was like unlocking a pie of steel chains holding down my corpse. It had some weird effect of switching a light in my head. If a doctor can feel imposter syndrome it's just human. I have to just accept the idea I wont know everything about something. The same vibe that got me hooked became a curse. So I keep asking questions. And asking friends who would like to join me in the adventure seeking answers. I have a friend who picked up code last week and it has really inspired me to get going. It is so cool to teach/ help people. Way better than money.
+</p>
+<img src="/img/wizardthree.jpg" alt="Mayday again">
+<p>
+  I've preached that we are not born knowing everything. Preached to anyone that I thought needed to hear it. But never listened to it come out of my mouth. So I looked at how to market and find clients. Just to see it as a refresher. Because in the years of learning how code works I'm also interested in the all of freelancing. How to make and build a client base as well as how to keep them around for the future. I've been trying to learn how to freelance without the actual step. I can only learn so much without trying to see what works for me.
+</p>
+<p><span>"I currently make less than twenty dollars an hour. In Washington state there is just not any room to live that low. Having to second guess a three dollar cheeseburger fucks me up."</span></p>
+<p>
+  So I pushed the initial dev page and life got busy. Not with clients. We started moving. I wish clients flooded as soon as I opened the door. But I don’t know what I would do if they did. In current activities I make excuses to hide from what I should do. Life has been a bunch of work between all of the things so I make excuses. The intention is to put up some boards like those political signs we see stuck in the ground on corners. And run some Google and Facebook ads. I will do what I can. I have to. I currently make less than twenty dollars an hour. In Washington state there is just not any room to live that low. Having to second guess a three dollar cheeseburger fucks me up.  Even failing is better than not trying at all. I would rather have tried then to look back and wonder what could have happened. That is remorse. 
+</p>
+<video controls src="/img/doggnut.mp4" ><track kind="captions"/></video>
+<p>
+  These first couple blogs have just been kind of a bit of rambling backstory and upcoming plans. The content will mostly contain what I have worked on through the week. Whatever I learned, links to tutorials or documents or any other information. It will probably evolve a bit as I figure out how to deliver what I want to deliver. Thanks for your patience as I work on this. 
+</p>
+
+<img src="/img/wizardfive.jpg" alt="the partner and I at the concert">
+
+</body>
+
+<style lang="scss"> 
+    
+    @import '../src/routes/_components.scss';
+
+    img{
+        max-width: 100vw;
+        margin: 0 15%;
+    }
+    video{
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .port {
+        max-width: 100vw;
+        margin: 0 40%;
+    }
+
+    audio{
+        margin: -9% 30% 0 30%;
+        box-shadow: 0 0 10px rgba(180, 10, 232, 0.5);
+        border-radius: 50%;
+    }
+
+    p{
+        margin: 9% 18%;
+
+        span{
+            font-weight: bold;
+            text-decoration: underline purple 2px;
+            text-transform: italic;
+            word-break: break-all;
+            letter-spacing: 2px;
+            font-size: 2.7vh;
+        }
+    }
+
+      @media (max-width: 768px) {
+      p{
+          margin: 9% 7%;
+      }
+      img{
+        max-width: 80vw;
+        margin: 5%;
+        }
+      .port {
+        max-width: 80vw;
+        margin: 4% 10%;
+        }
+      }
+</style>
