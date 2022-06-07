1529a378c5709fd9b97db26e7372894de3e502bb
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-06-07 10:33:55 -0700

    Add files via upload

---
+++ src/routes/blog/lostLostNotFound.svelte
@@ -0,0 +1,74 @@
+<body>
+	<h1>Lost Lost Not Found</h1>
+
+	<img class="cent" src="/blogPic/lostnotFound/one.png" alt="bread sandwich" />
+
+	<!--   <p>
+                Save your sight. You can listen instead.
+        </p>
+        <audio controls>
+        <source src="/sound/stillAliveMufuka.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+        </audio>
+-->
+
+	<p>don't waste your time reading this</p>
+
+	<p>
+		Sometimes it feels easier than melting snow on a bonfire. I wish it wasn't. I can just drop off
+		the earth from everything. It is not intentional. It kind of eats at me honestly. I want human
+		interaction, but I have been disconnected for so long that there is no reciprocated response. I
+		look to the socials and feel lost. I wish I could have found some kind of better coping skill to
+		deal with that obnoxious fear of missing out. I don’t want to give that feeling any energy. It
+		does not deserve it. That’s one of the reasons I stay so busy. I need to be productive so I
+		don’t get lost in myself. There is no found in the future. There is no sense of it will get
+		better. It only feels as if the place I am is where I belong. All of the choices I made led to
+		this day. I made this life that I wish was so much more. I thought that getting away from
+		socials would help me. But it only makes me feel more disconnected.
+	</p>
+	I don’t quite get it either. I was never the first person on a new social site. Much more commonly
+	the opposite had me the last of the bunch. Did that preschedule my fall from what a "happy life" looks
+	like? Is media just something for me to point my finger at? It all leans back into my problem being
+	me. I should be able to find minuscule bits of hope. But I just feel sick when I think of my future.
+	Guess it's good nobody gives a fuck to consume anything I create. Just what I need is some person who
+	pretends to want to care for their savior complex. It's not like I put anything out into the ether
+	for anyone to notice. Even if I did nobody would give a fuck. They have their own shit to deal with
+	over the ramblings of just another human.
+	<p>
+		This week I started a project for a musician friend of mine. To keep myself a little busy and
+		see how quickly I could throw something together. I have already spent so much time against
+		other walls that it seemed too easy. But it's just a draft. So maybe a start at 30% completed.
+		It reminds me why I want to build things in a 3d space. Because it feels like more than a bread
+		sandwich. Yes, that’s a real thing.
+	</p>
+	One of the bonuses to a new project is that I try new things. If I don’t learn something, why build?
+	Maybe if I was to the point of creating production line sites. This page will be svgs. I have made
+	them before, but I want to get a bit ok at them. I am going to order a cheap drawing tablet for work
+	stuff. There is so much that would be used with it. I have a touchpad. It's too bad I can't draw on
+	that. I will be able to use the tablet for blender and doing 3d stuff. It will pay for itself. Especially
+	after I net a client. I need to look for them. I need to build a plan of action and follow-through.
+	I am the only one I have to blame for being broke by now. The skills I have should have set me up.
+	If anything, my first chance I am to hire someone who is good at marketing and sales.
+	<p>
+		So I think while working on the project for Con I am going to start remoting to my computer to
+		build shit in 3d that my little laptop could actually cant do. Yes, I have so much love for this
+		little thing, but the moment I even think of doing anything extra it starts steaming out the
+		vents. I have an idea of making the background for his page in a 3d space instead of just a
+		color back.
+	</p>
+	I have been looking around on Amazon trying to buy some happiness and motivation. I keep looking for
+	hope only to come up empty-handed. I hunt for something that will not feel hollow. Unyielding any results
+	that last longer than five minutes, I attempt to fabricate that hope. Like some kind of synthesized
+	oxygen to keep my lungs pumping another few minutes. To give reason to my heart to want to beat. I
+	hold now for two children who have expectations and needs for their future. If I am nothing but their
+	thread to the world what am I to me? A placeholder of faux providence? I want to write but can't find
+	the want to write. It all comes out dark and dreary. I can't seem to shake that sick feeling every
+	time I get it. I don’t want this anymore. And I don’t know how to get better. I can't afford mental
+	health care, and the lack of being able to afford is a huge hindrance to happiness.
+	<p>I wonder how happy I would be if there was not the overbearing sense of not being enough.</p>
+</body>
+
+<style lang="scss">
+	@import '../src/routes/_components.scss';
+	@import 'guts';
+</style>
