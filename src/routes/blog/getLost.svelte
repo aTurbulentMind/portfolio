9fe5d3c00aca1f351b4c13b351c7c513aa2c1d4b
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-07-06 09:08:51 -0700

    Add files via upload

---
+++ src/routes/blog/getLost.svelte
@@ -0,0 +1,85 @@
+<body>
+	<h1>getLost!</h1>
+
+	<img src="/blogPic/goGetLost/one.png" alt="jaded or burnt out?" />
+
+	<p>
+		Getting lost in nature can be that ever so necessary keg stand of levelheadedness you're
+		questing for. It’s a low cost joy quenching activity. There is no tech for me in the middle of
+		nowhere. Well, let me be clear, no music, no social media, or podcasts or anything like that.
+		I'm not trying to go full bushmaster. I don’t give a fuck if nowhere is in the middle of town.
+		Surround me in trees, maybe line one side with shore and I am where I need to be. The kids will
+		be a great addition to those real vista points. I forget how important it is. How refueling it
+		is. My heart goes out to the people confined in the concrete jungle, the horizon is the closest
+		to a tree they get.
+	</p>
+	<img src="/blogPic/goGetLost/six.jpg" alt="feet up at a campsite" />
+	<p>
+		Let's cut into the week's work. Not a whole lot of learning. I have been processing Con's site.
+		I have done enough by now that a lot of my process is in my mental library. If anything it's
+		finding what works. A prime example is for merch on this page I'm working on, I wanted to have a
+		background image as a shirt and a variable inside that can be whatever image file that would be
+		on the shirt. The image overlay kept fighting me so I just made a tiny png that act as a button.
+		It does the thing I ask it so I will take it. Now, most of the list is checked off. I would like
+		to make a background in blender. Some drops of blood on the concrete surface would be great. And
+		maybe add a loading screen for whenever the site is taking more than three seconds.
+	</p>
+	<p>
+		That would be something worth the time to do. I try to at least learn a few things. This page
+		was definitely making SVG. That shit is so much fun. Now I just want to doodle and work on
+		design stuff all day. That frontend dev life.
+	</p>
+	<img src="/blogPic/goGetLost/two.png" alt="sale svg" />
+	<p>
+		It is starting to finally feel like summer around here. After the solstice we had our first day
+		over 70 degrees. We spend so many of our days under this overcast blanket grey. Day in and day
+		out. Just grey. It's oppressing. It can be easy to blend lines between my own inner turmoil and
+		the effect of the weather. That lack of vitamins from the sun is a killer. We have a backpacking
+		trip planned for the weekend. More of that outdoor reset we need so much. To a place in Eastern
+		Washington near Quincy called Ancient Lakes.
+	</p>
+	<p>
+		This week I have really attempted to put my mental space first. No thoughts of work shit while I
+		am not working. At least a few weeks to heal a bit from these damned burns to that squishy bit
+		of brain matter between your ears. For a long time I have thought my incessant need to be busy
+		all the time was a personal issue. Some error in my head from my actual mother being sedentary.
+		Over the last few weeks I have heard a few people mention their own need to be active at all
+		moments in the day. What a relief. Other people are going through the same problem. We know we
+		need to stop and have a moment but we feel that need to always be on. We in that category have
+		to learn to set boundaries at where we are comfortable. We deserve that much at least.
+	</p>
+	<img src="/blogPic/goGetLost/seven.jpg" alt="an old thunderbird" />
+	<p>
+		At the end of the week I sent out a social media life update. It was refreshing to see people
+		care a little bit. I even had a few friends reach out to me about their own interest or path to
+		web development. I love that. One of them is into music and wants to build a site for their
+		clothing and music. I love that because I started in that similar space. Wanting to make my own
+		product and having absolute control over the workings of it. We talked about writing code
+		together and that is great. I love the idea of working on something together with people. I love
+		the idea of making a great team of people all interested in growing mentally and financially
+		while helping people. It should help by finding people who came from nothing like myself. There
+		is often an itch in the back of our character to help lift others from the trauma we faced for
+		years or decades of our life.
+	</p>
+	<img src="/blogPic/goGetLost/four.jpg" alt="my face" />
+	<p>
+		Another to reach out has been a friend since Jr High. She has always been a super supportive
+		human around me and if I could, I want her on my team. She is super compassionate about animals
+		and came from a broken childhood like mine. I already know her to work with the Boy's and Girl's
+		group so know she has a kind heart. That matters. People that care about others are frequently
+		put at a lower rung for giving so much of themselves to the world. This world has no hesitation
+		to take everything it can and leave a shell of who you always wanted to be. We must refuse the
+		relinquishment of what we hold so close in the progression of our life.
+	</p>
+	<p>
+		The trip was great and I'll be writing about how life feels after the staycation we had. Tanks
+		are refilled and ready to be busy and productive.
+	</p>
+
+	<img src="/blogPic/goGetLost/five.jpg" alt="beautiful sunset in a canyon" />
+</body>
+
+<style lang="scss">
+	@import '../src/routes/_components.scss';
+	@import 'guts';
+</style>
