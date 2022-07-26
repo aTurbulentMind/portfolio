a940df53ffeef207437a5ad00c1b130374f7dc4f
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-07-26 08:51:32 -0700

    Add files via upload

---
+++ src/routes/blog/finalNote.svelte
@@ -0,0 +1,114 @@
+<body>
+	<h1>Final note</h1>
+
+	<p>
+		Fine tuning and practice makes better. The idea of perfect is ridiculous. Nobody should seek to
+		achieve a level of perfection. That is a deep cut I wish I would have taken account for years
+		ago. There is no perfect developer, there is no perfect website and people will always have an
+		opinion about what is and is not good. On the drive in to work today I was thinking of where my
+		agency and page could be if I had just leapt into the unknown. I have made a few of my own
+		pages. Starting from the ground up each time. Thinking it would be some type of way better then
+		just progressing so slowly.
+	</p>
+	<p>
+		I spent yesterday cleaning up SVG on the tattoo page and getting my own page all well rounded.
+		Every day I am having a harder and harder time keeping fueled with motivation.
+	</p>
+	<p>
+		I need some wins in my life. I need some kind of leeway. This sick feeling never really goes
+		away anymore. It is just a waiting period to the next bout of battling with myself. It gets
+		heavy and I'm getting tired of lifting this weight. I don’t want it anymore. I see no other
+		option. I see no other way for me to get out. I don't think I want to blog anymore. I am not
+		receiving any mental gratification. Its just another thing to spend time on that I lose interest
+		in. I would pay to care by now. If it would make me a bit more happy. I wouldn’t lose a wink of
+		sleep the cost. Ha. I know that’s a lie because I'm so economic.
+	</p>
+	<p>I'm tired.</p>
+	<p>
+		I want to participate in society. I want to have friends and circles of people across the
+		different social platforms. Don’t be like me. I open an app, with full knowledge of how it
+		impacts us as a species. I know these companies feed bullshit to the people. I know they collect
+		and sell our data and information while they build a dossier on how to sell you things before
+		you know you want it. I am not in favor of the great algorithm gods. I don’t do trendy shit. So
+		anything I do share is invisible to the world. Unless I pay to boost it through the scroll.
+	</p>
+	<p>
+		Each platform has its own reason to not like it. So I shut away. The options are not hard to
+		measure out on the scale. They make me feel sick so I tend to stay away. I reach out to people I
+		have called friend over time to only catch silence. So I stopped bothering people. Which makes
+		me nauseas. Is it not the algorithm? Is it actually me that is the problem? If people unaffected
+		by the algorithm don’t want to talk to me, it has to be me. So if I don’t want to be alive and
+		the world does not care for me to exist why am I here? I feel like a waste of societies
+		resources. I say I'm here for my kids, but how much better would their life be without me. How
+		much trauma would be saved from my lack of parenting skills. Instead of hope in their father
+		they would have nothing to let them down. Its getting harder to hang on to this cliff.
+	</p>
+	<p>
+		I wanted to be here for them for years. I held on to the hope that I could do something to make
+		their life and future better. I lost control of that idea a few weeks ago. I don’t see hope in
+		my future. I see pain. I see suffering that wont change. If I look back through the thirty some
+		years I've been here already, how do I find any hope that the suffering will be any different
+		over the next thirty. Poverty and lacking the self love to care about existing. Living and not
+		being able to take the steps I know I need is a daily prison. I lock myself away in a shell that
+		moves in the motive of how people expect me to be. There are real world depictions of great
+		parents, I'll never be that.
+	</p>
+	<p>
+		I don’t live for me. I don’t want to live. I am just pretending too. I am here so a few people
+		will not go through the pain of me removing myself from our population. Is that any reason to
+		take a breath? I don’t think so. I want to want to wake up. I want to desire the things of life,
+		but they only make me nauseas. A few hits of serotonin between the troubling times made
+		everything worth it. Now I feel grateful to the night for giving me the closest taste of death
+		in letting sleep take me. I miss loving to wake up to have a chance at another day.
+	</p>
+	<p>
+		I disservice my partner by depriving memories that could have been the best of her life. I lose
+		my sense of having fun and find it so difficult to be in the moment which robs us of happy
+		memories. She could have such a better life without me holding her down, she claims otherwise. I
+		think to all of the times when I was trash as a person. I do not always act the best. She
+		deserves better than I could ever be.
+	</p>
+	<p>
+		I cant apply myself and I am dying because of it. I have spent four years learning different
+		things about web development and still don’t make much more than minimum wage. More proof that
+		society deems me unworthy of life. Our local cost of living is more than $12 an hour higher then
+		I make. All because I cant take action on the things I've learned. Its my fault that I am not
+		any further. So why would I try? Do I spend four more years of minimum wage jobs while people in
+		six months make five times my wages?
+	</p>
+	<p>
+		I know what it takes to get my life right. I have a list of things that would improve my
+		everyday. I have zero energy or actual motivation to make them happen. Why? Why cant I do what
+		it takes to make my life better? Is it that I don’t deserve it and know it. Using development as
+		a bottomless pit to throw my consciousness into? Are all of these activities to keep me away
+		from enjoying my last day?
+	</p>
+	<p>
+		I'm not good. And I don’t feel like I will get better. I've had periods where there was a bounce
+		back. A return to motivation. I don’t know that I will find it again. I don’t see much point in
+		trying or pretending to want to. I don’t have anyone I can talk to really. And what would change
+		if I did. What would become different? People would think even less of me. They would look with
+		a pointing finger claiming me a loser. If the shoe fits. I have not done shit with my life to
+		argue their point. In fact I leaned in to being worthless. I just don’t have the energy for
+		trying anymore. Just running on autopilot so I don’t have time to pick up however I choose to go
+		out.
+	</p>
+</body>
+
+<style lang="scss">
+	@import '../src/routes/_components.scss';
+	@import 'guts';
+
+	span {
+		font-style: italic;
+	}
+
+	@media (max-width: 991px) {
+	}
+
+	@media (max-width: 768px) {
+	}
+
+	@media (min-width: 750px) {
+	}
+</style>
