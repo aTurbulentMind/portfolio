f0dcd3d7335ff0d72a10c624e2545eca62cf31a2
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-10-03 10:57:12 -0700

    Add files via upload

---
+++ build/_app/immutable/chunks/6-8cdc12c6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/testim/_page.svelte-df75fbdd.js";export{t as component};
---
+++ build/_app/immutable/components/pages/about/_page.svelte-182cff50.js
@@ -0,0 +1,33 @@
+import{S as ne,i as se,s as re,k as r,a as i,r as u,l as o,m as c,c as l,u as d,h,p as a,I as E,q as oe,b as ie,D as e,n as C}from"../../../chunks/index-bde843fc.js";function le(ae){let t,f,K,P,y,S,x,m,A,T,M,O,b,N,R,w,j,q,g,L,z,v,F,G,k,H,B,p,Q,D,_,J,Y,s,U;return{c(){t=r("body"),f=r("img"),P=i(),y=r("h1"),S=u("Joel Rivers"),x=i(),m=r("p"),A=u(`Age: 32\r
+		`),T=r("br"),M=u(`\r
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas\r
+		and some backpacking.`),O=i(),b=r("p"),N=u(`I am a thirty-two year old from western Washington. My family has been in the area for a few\r
+		generations by now. With a new branch of the family tree under me carried by my two little best\r
+		friends. The area and community of Washington make it easy to live here and never leave. Travel\r
+		is fun but home is on the waters of the Puget Sound.`),R=i(),w=r("p"),j=u(`I picked up website development a few years ago. I have done it for fun, but I want to use my\r
+		skills to help people who need it. With great power comes great responsibility? While working in\r
+		a retail store, I spent down time learning about building a new website for the shop. Something\r
+		magic happened and I easily found myself addicted. One realm of learning bled into another. As\r
+		the wealth of knowledge grew, so did the intrest in more.`),q=i(),g=r("img"),z=i(),v=r("p"),F=u(`Today I find myself entranced by how I can help whoever I can with what makes me so happy. Not\r
+		only does it benefit the recipient, but I get to become that much better along the way.`),G=i(),k=r("p"),H=u(`I do spend some time with fun things like hiking and backpacking to get away and reset the brain\r
+		from being immersed in technology. There is something magic about the serenity that is the\r
+		nature of the Pacific Northwest.`),B=i(),p=r("img"),D=i(),_=r("p"),J=u(`Or to unwind I may be caught spinning fire. As the world surrounding me slows at the crackling\r
+		flint stored in the bic. The rush of endorfins pumping through the veins to the endpoints of\r
+		each extremity. Within microseconds, the combustion seranades my soul with zen. The most\r
+		prominent feeling of "this is peace" that I can access without a four hour trek.`),Y=i(),s=r("iframe"),this.h()},l(I){t=o(I,"BODY",{class:!0});var n=c(t);f=o(n,"IMG",{class:!0,src:!0,alt:!0}),P=l(n),y=o(n,"H1",{class:!0});var V=c(y);S=d(V,"Joel Rivers"),V.forEach(h),x=l(n),m=o(n,"P",{class:!0});var W=c(m);A=d(W,`Age: 32\r
+		`),T=o(W,"BR",{class:!0}),M=d(W,`\r
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas\r
+		and some backpacking.`),W.forEach(h),O=l(n),b=o(n,"P",{class:!0});var X=c(b);N=d(X,`I am a thirty-two year old from western Washington. My family has been in the area for a few\r
+		generations by now. With a new branch of the family tree under me carried by my two little best\r
+		friends. The area and community of Washington make it easy to live here and never leave. Travel\r
+		is fun but home is on the waters of the Puget Sound.`),X.forEach(h),R=l(n),w=o(n,"P",{class:!0});var Z=c(w);j=d(Z,`I picked up website development a few years ago. I have done it for fun, but I want to use my\r
+		skills to help people who need it. With great power comes great responsibility? While working in\r
+		a retail store, I spent down time learning about building a new website for the shop. Something\r
+		magic happened and I easily found myself addicted. One realm of learning bled into another. As\r
+		the wealth of knowledge grew, so did the intrest in more.`),Z.forEach(h),q=l(n),g=o(n,"IMG",{src:!0,alt:!0,class:!0}),z=l(n),v=o(n,"P",{class:!0});var $=c(v);F=d($,`Today I find myself entranced by how I can help whoever I can with what makes me so happy. Not\r
+		only does it benefit the recipient, but I get to become that much better along the way.`),$.forEach(h),G=l(n),k=o(n,"P",{class:!0});var ee=c(k);H=d(ee,`I do spend some time with fun things like hiking and backpacking to get away and reset the brain\r
+		from being immersed in technology. There is something magic about the serenity that is the\r
+		nature of the Pacific Northwest.`),ee.forEach(h),B=l(n),p=o(n,"IMG",{src:!0,alt:!0,class:!0}),D=l(n),_=o(n,"P",{class:!0});var te=c(_);J=d(te,`Or to unwind I may be caught spinning fire. As the world surrounding me slows at the crackling\r
+		flint stored in the bic. The rush of endorfins pumping through the veins to the endpoints of\r
+		each extremity. Within microseconds, the combustion seranades my soul with zen. The most\r
+		prominent feeling of "this is peace" that I can access without a four hour trek.`),te.forEach(h),Y=l(n),s=o(n,"IFRAME",{class:!0,width:!0,height:!0,style:!0,src:!0,title:!0,frameborder:!0,allow:!0}),c(s).forEach(h),n.forEach(h),this.h()},h(){a(f,"class","face svelte-bkco5y"),E(f.src,K="img/myFace.jpg/")||a(f,"src",K),a(f,"alt","its my face"),a(y,"class","svelte-bkco5y"),a(T,"class","svelte-bkco5y"),a(m,"class","svelte-bkco5y"),a(b,"class","svelte-bkco5y"),a(w,"class","svelte-bkco5y"),E(g.src,L="img/nature2.jpg/")||a(g,"src",L),a(g,"alt","sunrays cast though morning fog"),a(g,"class","svelte-bkco5y"),a(v,"class","svelte-bkco5y"),a(k,"class","svelte-bkco5y"),E(p.src,Q="img/nature1.jpg/")||a(p,"src",Q),a(p,"alt","a sunny day on top of a mountain at a lake. It is serene"),a(p,"class","svelte-bkco5y"),a(_,"class","svelte-bkco5y"),a(s,"class","vid svelte-bkco5y"),a(s,"width","560"),a(s,"height","315"),oe(s,"margin-bottom","120px"),E(s.src,U="https://www.youtube.com/embed/zEs79S5yTr0?controls=0/")||a(s,"src",U),a(s,"title","YouTube video player"),a(s,"frameborder","0"),a(s,"allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"),s.allowFullscreen=!0,a(t,"class","svelte-bkco5y")},m(I,n){ie(I,t,n),e(t,f),e(t,P),e(t,y),e(y,S),e(t,x),e(t,m),e(m,A),e(m,T),e(m,M),e(t,O),e(t,b),e(b,N),e(t,R),e(t,w),e(w,j),e(t,q),e(t,g),e(t,z),e(t,v),e(v,F),e(t,G),e(t,k),e(k,H),e(t,B),e(t,p),e(t,D),e(t,_),e(_,J),e(t,Y),e(t,s)},p:C,i:C,o:C,d(I){I&&h(t)}}}class ce extends ne{constructor(t){super(),se(this,t,null,le,re,{})}}export{ce as default};
