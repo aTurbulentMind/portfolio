5267ae0003d1b96b82aacb5d089b280836c1076a
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-10-03 10:43:36 -0700

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,88 @@
+<body>
+	<img class="face" src="img/myFace.jpg/" alt="its my face" />
+
+	<h1>Joel Rivers</h1>
+
+	<p>
+		Age: 32
+		<br />
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas
+		and some backpacking.
+	</p>
+
+	<p>
+		I am a thirty-two year old from western Washington. My family has been in the area for a few
+		generations by now. With a new branch of the family tree under me carried by my two little best
+		friends. The area and community of Washington make it easy to live here and never leave. Travel
+		is fun but home is on the waters of the Puget Sound.
+	</p>
+
+	<p>
+		I picked up website development a few years ago. I have done it for fun, but I want to use my
+		skills to help people who need it. With great power comes great responsibility? While working in
+		a retail store, I spent down time learning about building a new website for the shop. Something
+		magic happened and I easily found myself addicted. One realm of learning bled into another. As
+		the wealth of knowledge grew, so did the intrest in more.
+	</p>
+
+	<img src="img/nature2.jpg/" alt="sunrays cast though morning fog" />
+
+	<p>
+		Today I find myself entranced by how I can help whoever I can with what makes me so happy. Not
+		only does it benefit the recipient, but I get to become that much better along the way.
+	</p>
+
+	<p>
+		I do spend some time with fun things like hiking and backpacking to get away and reset the brain
+		from being immersed in technology. There is something magic about the serenity that is the
+		nature of the Pacific Northwest.
+	</p>
+
+	<img src="img/nature1.jpg/" alt="a sunny day on top of a mountain at a lake. It is serene" />
+
+	<p>
+		Or to unwind I may be caught spinning fire. As the world surrounding me slows at the crackling
+		flint stored in the bic. The rush of endorfins pumping through the veins to the endpoints of
+		each extremity. Within microseconds, the combustion seranades my soul with zen. The most
+		prominent feeling of "this is peace" that I can access without a four hour trek.
+	</p>
+
+	<iframe
+		class="vid"
+		width="560"
+		height="315"
+		style="margin-bottom: 120px;"
+		src="https://www.youtube.com/embed/zEs79S5yTr0?controls=0/"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+	/>
+</body>
+
+<style lang="scss">
+	@import '../src/lib/basecamp';
+
+	img {
+		width: 80%;
+		margin: 0 10%;
+		border-radius: 4px;
+	}
+
+	ul {
+		list-style: none;
+		margin: 7%;
+		font-size: 2.25vw;
+	}
+
+	.vid {
+		max-width: 99%;
+	}
+
+	@media (min-width: 700px) {
+		.face {
+			width: 35%;
+			margin: 4% 35%;
+		}
+	}
+</style>
