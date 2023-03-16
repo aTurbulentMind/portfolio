7cc97c99cb0fbbbcd78dc4a606684b3503fadecc
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2023-03-16 11:01:32 -0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	ssr: {noExternal: ['three', 'troika-three-text']}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
