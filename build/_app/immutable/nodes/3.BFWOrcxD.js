f5db94320d947609cf9dcff5d8fc0078695daf33
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-19 10:42:50 -0700

    Delete build directory

--- build/_app/immutable/nodes/3.BFWOrcxD.js
+++
@@ -1,9 +0,0 @@
-import{q as h,t as e,u,v as d}from"../chunks/disclose-version.CEDXH0Ax.js";import{a as t}from"../chunks/attributes.CpwgvkvO.js";const c=""+new URL("../assets/myFace.C1997zoo.jpg",import.meta.url).href,r=""+new URL("../assets/nature1.CB9CZlpn.jpg",import.meta.url).href;var f=d(`<img class="face svelte-8bq1q4" alt="its my face"> <h1>Joel Rivers</h1> <p>I am a thirty-four year old from Western Washington.The Kitsap area and community of Washington
-	make it easy to live here and never leave. Travel is fun but home is the forest surrounding the
-	Puget Sound.</p> <img alt="a sunny day on top of a mountain at a lake. It is serene" class="svelte-8bq1q4"> <p>I picked up website development a few years ago. While working in a retail store. I spent down
-	time learning about building a new website for the shop. Something magic happened and I easily
-	found myself addicted. One realm of learning bled into another. As the wealth of knowledge grew,
-	so did the interest in learning more. I've done it for fun, but I want to use my skills to help
-	people who need it.</p> <p>I do spend some time with fun things like going to concerts or playing video games. To get away
-	and reset the brain. Hiking and backpacking is another nice escape from being immersed in
-	technology. There is something magic living in the wild serenity of the Pacific Northwest.</p> <img alt="sunrays cast though morning fog" class="svelte-8bq1q4">`,1);function y(o){var a=f(),n=h(a);t(n,"src",c);var s=e(e(n,!0)),l=e(e(s,!0)),i=e(e(l,!0));t(i,"src",r);var m=e(e(i,!0)),g=e(e(m,!0)),p=e(e(g,!0));t(p,"src",r),u(o,a)}export{y as component};
