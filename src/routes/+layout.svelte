383e02b0b5f76073ef1de27ac0e8502371f25864
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-20 13:29:50 -0700

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '$lib/styles/baseCamp.svelte';
+	import Nav from '$lib/Nav.svelte';
+	import Footer from '$lib/Footer.svelte';
+</script>
+
+<Nav />
+<slot />
+
+<Footer />
+
+<!--svelte-ignore css-unused-selector -->
+<style></style>
