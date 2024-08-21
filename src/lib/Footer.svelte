09bd042d7a08fb09c96b985880d2c40276abbc20
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-21 09:20:41 -0700

    Add files via upload

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,37 @@
+<footer>
+	<p>
+		Produced by <a href="https://www.lanternlightdevelopment.com/"> Lantern Light Development</a>
+	</p>
+</footer>
+
+<!-- svelte-ignore css-unused-selector -->
+<style>
+	footer {
+		background-color: var(--back_Alt);
+		display: none;
+		justify-content: center;
+		position: relative;
+		bottom: 0;
+		width: 100vw;
+		height: 4vh;
+	}
+
+	p {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: var(--f_sm);
+		margin: var(--marg);
+		gap: 10px;
+	}
+
+	a {
+		color: var(--back_Main);
+	}
+
+	@media only screen and (min-width: 980px) {
+		p {
+			font-size: var(--f_m);
+		}
+	}
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import Twitt from '$lib/svg/birdy.svelte';
+	import Insta from '$lib/svg/insta.svelte';
+	import FaBoo from '$lib/svg/faBoo.svelte';
+</script>
+
+<div class=" head_Line">
+	<h1>Contact:</h1>
+</div>
+
+<div class="follow">
+	<a href="https://twitter.com/LanternLightDev/"> <svg><Twitt /> </svg></a>
+	<a href="https://www.instagram.com/lanternlightdevelopment/"><svg> <Insta /></svg> </a>
+	<a href="https://www.facebook.com/profile.php?id=100078803221612/"><svg><FaBoo /></svg> </a>
+</div>
+
+<!-- 
+<container>
+	<form action="https://formsubmit.co/joelkrivers@gmail.com" method="POST">
+		<wrapper>
+			<box>
+				<input type="text" name="name" id="name" required />
+				<label for="fname">First & Last Name</label>
+			</box>
+		</wrapper>
+
+		<wrapper>
+			<box>
+				<input type="text" name="email" id="email" required />
+				<input type="hidden" name="_next" value="https://www.lanternlightdevelopment.com/contact" />
+				<label for="email">Email</label>
+			</box>
+		</wrapper>
+
+		<wrapper>
+			<box>
+				<input type="text" name="tel" id="tel" required />
+				<label for="tel">Preferred communication</label>
+			</box>
+		</wrapper>
+
+		<wrapper>
+			<box>
+				<input type="text" name="comp" id="comp" required />
+				<label for="comp">Company/Entity Name</label>
+			</box>
+		</wrapper>
+
+		<wrapper>
+			<box>
+				<textarea id="mes" required></textarea>
+				<label for="mes">Comments or Questions:</label>
+			</box>
+		</wrapper>
+
+		<button type="submit">Submit</button>
+	</form>
+</container> -->
+
+<!--svelte-ignore css-unused-selector -->
+<style></style>
