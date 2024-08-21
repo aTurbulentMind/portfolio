22edbdd4e1b1125dcb05ff48881939f847e7d20d
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-21 08:54:36 -0700

    Add files via upload

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,60 @@
+<body>
+	<img class="face" src="img/myFace.jpg" alt="its my face" />
+
+	<h1>Joel Rivers</h1>
+
+	<p>
+		Age: 33
+		<br />
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas
+		and some backpacking.
+	</p>
+
+	<p>
+		I am a thirty-three year old from Western Washington.The area and community of Washington make
+		it easy to live here and never leave. Travel is fun but home is on the waters of the Puget
+		Sound.
+	</p>
+
+	<img src="img/nature1.jpg" alt="a sunny day on top of a mountain at a lake. It is serene" />
+
+	<p>
+		I picked up website development a few years ago. While working in a retail store. I spent down
+		time learning about building a new website for the shop. Something magic happened and I easily
+		found myself addicted. One realm of learning bled into another. As the wealth of knowledge grew,
+		so did the interest in learning more. I've done it for fun, but I want to use my skills to help
+		people who need it.
+	</p>
+
+	<p>
+		Now I am deep into building 3d websites. Currently working on a new 3d portfolio page. It is so
+		much fun. And if a giant company succeeds in adoption of their mixed reality headsets, we will
+		need spatial worlds to explore beyond our old flat places of simple scrolling.
+	</p>
+
+	<p>
+		I do spend some time with fun things like going to concerts or playing video games. To get away
+		and reset the brain. Hiking and backpacking is another nice escape from being immersed in
+		technology. There is something magic living in the wild serenity of the Pacific Northwest.
+	</p>
+
+	<img src="img/nature2.jpg" alt="sunrays cast though morning fog" />
+</body>
+
+<!--svelte-ignore css-unused-selector -->
+
+<style lang="scss">
+	@import '../src/lib/basecamp';
+
+	img {
+		width: 80%;
+		margin: 0 10%;
+	}
+
+	@media (min-width: 700px) {
+		.face {
+			width: 35%;
+			margin: 4% 35%;
+		}
+	}
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,69 @@
+<body>
+	<h1>Contact:</h1>
+
+	<div class="follow">
+		<a href="https://twitter.com/LanternLightDev/">Twitter(X)</a>
+		<a href="https://www.instagram.com/lanternlightdevelopment/">Instagram</a>
+		<a href="https://www.facebook.com/profile.php?id=100078803221612/">Facebook</a>
+	</div>
+
+	<container>
+		<form action="https://formsubmit.co/joelkrivers@gmail.com" method="POST">
+			<wrapper>
+				<box>
+					<input type="text" name="name" id="name" required />
+					<label for="fname">First & Last Name</label>
+				</box>
+			</wrapper>
+
+			<wrapper>
+				<box>
+					<input type="text" name="email" id="email" required />
+					<input
+						type="hidden"
+						name="_next"
+						value="https://www.lanternlightdevelopment.com/contact"
+					/>
+					<label for="email">Email</label>
+				</box>
+			</wrapper>
+
+			<wrapper>
+				<box>
+					<input type="text" name="tel" id="tel" required />
+					<label for="tel">Preferred communication</label>
+				</box>
+			</wrapper>
+
+			<wrapper>
+				<box>
+					<input type="text" name="comp" id="comp" required />
+					<label for="comp">Company/Entity Name</label>
+				</box>
+			</wrapper>
+
+			<wrapper>
+				<box>
+					<textarea id="mes" required />
+					<label for="mes">Comments or Questions:</label>
+				</box>
+			</wrapper>
+
+			<button type="submit">Submit</button>
+		</form>
+	</container>
+</body>
+
+<!--svelte-ignore css-unused-selector -->
+<style lang="scss">
+	@import '../src/lib/basecamp';
+	* {
+		box-sizing: border-box;
+	}
+
+	container {
+		form {
+			margin-bottom: 10vh;
+		}
+	}
+</style>
