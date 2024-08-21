51dd678c0df582384274dc67d447c4713668752f
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-21 09:47:36 -0700

    Add files via upload

---
+++ build/_app/immutable/entry/about-page.svelte.283984ba.js
@@ -0,0 +1,33 @@
+import{S as U,i as V,s as X,k as r,a as l,r as b,l as i,m as g,c,u as v,h,p as n,N as T,q as Z,b as $,F as t,n as G}from"../chunks/index.d409ffe5.js";function ee(Q){let e,m,N,W,p,E,P,o,S,_,x,A,f,M,H,d,B,O,y,F,R,u,J,j,w,q,z,s,Y;return{c(){e=r("body"),m=r("img"),W=l(),p=r("h1"),E=b("Joel Rivers"),P=l(),o=r("p"),S=b(`Age: 32\r
+		`),_=r("br"),x=b(`\r
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas\r
+		and some backpacking.`),A=l(),f=r("p"),M=b(`I am a thirty-two year old from Western Washington. My family has been in the area for a few\r
+		generations by now. With a new branch carried by my two little best friends. The area and\r
+		community of Washington make it easy to live here and never leave. Travel is fun but home is on\r
+		the waters of the Puget Sound.`),H=l(),d=r("img"),O=l(),y=r("p"),F=b(`I picked up website development a few years ago. While working in a retail store, I spent down\r
+		time learning about building a new website for the shop. Something magic happened and I easily\r
+		found myself addicted. One realm of learning bled into another. As the wealth of knowledge grew,\r
+		so did the interest in more. I've done it for fun, but I want to use my skills to help people\r
+		who need it. I do spend some time with fun things like going to concerts or playing video games.\r
+		To get away and reset the brain. Hiking and backpacking is another nice escape from being\r
+		immersed in technology. There is something magic living in the wild serenity of the Pacific\r
+		Northwest.`),R=l(),u=r("img"),j=l(),w=r("p"),q=b(`Or to unwind I may be caught spinning fire. As the world surrounding me slows at the crackling\r
+		flint stored in the bic. The rush of endorphins pumping through the veins to the endpoints of\r
+		each extremity. Within microseconds, the combustion serenades my soul with zen. The most\r
+		prominent feeling of "this is peace" that I can access without a four hour trek.`),z=l(),s=r("iframe"),this.h()},l(k){e=i(k,"BODY",{class:!0});var a=g(e);m=i(a,"IMG",{class:!0,src:!0,alt:!0}),W=c(a),p=i(a,"H1",{class:!0});var C=g(p);E=v(C,"Joel Rivers"),C.forEach(h),P=c(a),o=i(a,"P",{class:!0});var I=g(o);S=v(I,`Age: 32\r
+		`),_=i(I,"BR",{class:!0}),x=v(I,`\r
+		Hobbies: Spinning poi, making music, making fancy food, adventure in both urban and forest areas\r
+		and some backpacking.`),I.forEach(h),A=c(a),f=i(a,"P",{class:!0});var D=g(f);M=v(D,`I am a thirty-two year old from Western Washington. My family has been in the area for a few\r
+		generations by now. With a new branch carried by my two little best friends. The area and\r
+		community of Washington make it easy to live here and never leave. Travel is fun but home is on\r
+		the waters of the Puget Sound.`),D.forEach(h),H=c(a),d=i(a,"IMG",{src:!0,alt:!0,class:!0}),O=c(a),y=i(a,"P",{class:!0});var K=g(y);F=v(K,`I picked up website development a few years ago. While working in a retail store, I spent down\r
+		time learning about building a new website for the shop. Something magic happened and I easily\r
+		found myself addicted. One realm of learning bled into another. As the wealth of knowledge grew,\r
+		so did the interest in more. I've done it for fun, but I want to use my skills to help people\r
+		who need it. I do spend some time with fun things like going to concerts or playing video games.\r
+		To get away and reset the brain. Hiking and backpacking is another nice escape from being\r
+		immersed in technology. There is something magic living in the wild serenity of the Pacific\r
+		Northwest.`),K.forEach(h),R=c(a),u=i(a,"IMG",{src:!0,alt:!0,class:!0}),j=c(a),w=i(a,"P",{class:!0});var L=g(w);q=v(L,`Or to unwind I may be caught spinning fire. As the world surrounding me slows at the crackling\r
+		flint stored in the bic. The rush of endorphins pumping through the veins to the endpoints of\r
+		each extremity. Within microseconds, the combustion serenades my soul with zen. The most\r
+		prominent feeling of "this is peace" that I can access without a four hour trek.`),L.forEach(h),z=c(a),s=i(a,"IFRAME",{class:!0,width:!0,height:!0,style:!0,src:!0,title:!0,frameborder:!0,allow:!0}),g(s).forEach(h),a.forEach(h),this.h()},h(){n(m,"class","face svelte-1275pey"),T(m.src,N="img/myFace.jpg")||n(m,"src",N),n(m,"alt","its my face"),n(p,"class","svelte-1275pey"),n(_,"class","svelte-1275pey"),n(o,"class","svelte-1275pey"),n(f,"class","svelte-1275pey"),T(d.src,B="img/nature1.jpg")||n(d,"src",B),n(d,"alt","a sunny day on top of a mountain at a lake. It is serene"),n(d,"class","svelte-1275pey"),n(y,"class","svelte-1275pey"),T(u.src,J="img/nature2.jpg")||n(u,"src",J),n(u,"alt","sunrays cast though morning fog"),n(u,"class","svelte-1275pey"),n(w,"class","svelte-1275pey"),n(s,"class","vid svelte-1275pey"),n(s,"width","560"),n(s,"height","315"),Z(s,"margin-bottom","120px"),T(s.src,Y="https://www.youtube.com/embed/zEs79S5yTr0?controls=0/")||n(s,"src",Y),n(s,"title","YouTube video player"),n(s,"frameborder","0"),n(s,"allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"),s.allowFullscreen=!0,n(e,"class","svelte-1275pey")},m(k,a){$(k,e,a),t(e,m),t(e,W),t(e,p),t(p,E),t(e,P),t(e,o),t(o,S),t(o,_),t(o,x),t(e,A),t(e,f),t(f,M),t(e,H),t(e,d),t(e,O),t(e,y),t(y,F),t(e,R),t(e,u),t(e,j),t(e,w),t(w,q),t(e,z),t(e,s)},p:G,i:G,o:G,d(k){k&&h(e)}}}class ne extends U{constructor(e){super(),V(this,e,null,ee,X,{})}}export{ne as default};
