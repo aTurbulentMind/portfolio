f0858d73eaf42ad37f11f72fca78e9caf531dff1
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2024-08-21 09:08:12 -0700

    Add files via upload

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Contact:</h1>
+
+<div class="follow">
+	<a href="https://twitter.com/LanternLightDev/">Twitter(X)</a>
+	<a href="https://www.instagram.com/lanternlightdevelopment/">Instagram</a>
+	<a href="https://www.facebook.com/profile.php?id=100078803221612/">Facebook</a>
+</div>
+
+<!--svelte-ignore css-unused-selector -->
+<style></style>
